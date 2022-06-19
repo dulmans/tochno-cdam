ce561be7a225b75092020ebc26eb628d8fbfd7fe
ulmans <abramovxz@gmail.com>
2022-06-19 10:56:37 +0300

    Finish traffic lights animation and edit random-color site-title interactive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 /* Интерактив клика на aside title (название сайта) */
-document.querySelector('.aside-title').addEventListener('click', (e) => {
-    const randomColor = ['#F61717', '#FFBF00', '#57D9AD', '#f44336', '#e91e63','#9c27b0', '#673ab7', '#00bcd4', '#8bc34a', '#ffeb3b', '#ffc107', '#ff5722'];
-    document.querySelector('.aside-title').style.color = randomColor[Math.floor(Math.random() * ++randomColor.length)];
-})
-
-/* Анимация светофора в aside */
 {
-    const red = document.querySelector('.traffic-lights__red');
-    const yellow = document.querySelector('.traffic-lights__yellow');
-    const green = document.querySelector('.traffic-lights__green');
-    red.addEventListener('click', (e) => {
-        green.classList.toggle('animstop')
+    let update = 0;
+    const elem = document.querySelector('.aside-title');
+    elem.addEventListener('click', () => {
+        const randomColor = ['#F61717', '#FFBF00', '#57D9AD', '#f44336', '#e91e63','#9c27b0', '#673ab7', '#00bcd4', '#8bc34a', '#ffeb3b', '#ffc107', '#ff5722'];
+        elem.style.color = randomColor[Math.floor(Math.random() * ++randomColor.length)];
+        update++;
+        setTimeout(() => {
+            update--;
+            if(update === 0){
+                elem.style.color = 'inherit';
+            };
+        }, 3000);
     })
 }
