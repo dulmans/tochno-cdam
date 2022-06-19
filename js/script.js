b348a827a182b5e103fa231ee8caeb2cf0485891
ulmans <abramovxz@gmail.com>
2022-06-19 12:06:57 +0300

    Finished layout ASIDE-bar, edit random-color animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* Интерактив клика на aside title (название сайта) */
 {
     let update = 0;
-    const elem = document.querySelector('.aside-title');
+    const elem = document.querySelector('.onclickevent-color');
     elem.addEventListener('click', () => {
         const randomColor = ['#F61717', '#FFBF00', '#57D9AD', '#f44336', '#e91e63','#9c27b0', '#673ab7', '#00bcd4', '#8bc34a', '#ffeb3b', '#ffc107', '#ff5722'];
         elem.style.color = randomColor[Math.floor(Math.random() * ++randomColor.length)];
