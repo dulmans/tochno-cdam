52964c78049195108df8dad0ea6488cb0b4df96d
ulmans <abramovxz@gmail.com>
2022-07-04 11:06:13 +0300

    Start layout and codding custom select-box (in test-folder)

--- js/json.js
+++ js/json.js
@@ -1,3 +1,1 @@
-import ticket from '../question/question.json' assert {type: 'json'};
-
-console.log(ticket[0].quis[0].point[0]);
+import ticket from '../question.json' assert {type: 'json'};
--- js/page.js
+++ js/page.js
@@ -4,7 +4,7 @@ function loadPage (namePage) {
             return `
             <div class="content-container content-box__center">
                 <div class="content-box home-page__content-box">
-                    <h2 class="content__title title">Добро пожаловать на портал</h2>
+                    <h2 class="content__title title">Добро пожаловать на проект</h2>
                     <h2 class="content__name-site title random-word">ТочноСдам.ru</h2>
                     <div class="text-box home-page__text-box">
                         <p class="content__paragraph paragraph">На нашем портале вы сможете тренироваться в решении билетов ГИБДД. <br/> Каждый раз вопросы генерируются согласно регламенту ГИБДД. <br/> То есть, наш тренажер полностью симулирует программу, в которой вы будете сдавать экзамен в ГАИ.</p>
