af0ffd2f8eaad585a168b1faeb6c06f62436f4ef
ulmans <abramovxz@gmail.com>
2022-07-07 02:30:49 +0300

    Add func choice nested-option

--- dev-test-folder/script.js
+++ dev-test-folder/script.js
@@ -1,24 +1,64 @@
-const formElem = document.getElementById('select-app');
-const formContElem = formElem.querySelector('.select-box')
-const titleCheckbox = formElem.querySelector('.title-box--open__options');
-const optionsElems = formElem.querySelector('.options');
-const optionInputElems = optionsElems.querySelectorAll('.option-input');
-const nestedSensElems = optionsElems.querySelectorAll('.option[data-nested]');
+document.querySelectorAll('.select-app').forEach(formElem => {
+    const formContElem = formElem.querySelector('.select-box')
+    const titleCheckbox = formElem.querySelector('.title-box--open__options');
+    const optionsElem = formElem.querySelector('.options');
+    const optionInputElems = optionsElem.querySelectorAll('.option-input');
+    const optionElems = formElem.querySelectorAll('.option-hov');
 
-titleCheckbox.addEventListener('change', function(){
-    formContElem.classList.toggle('active');
-});
-
-optionInputElems.forEach(elem => {
-    elem.addEventListener('change', function (){
-        if(this.checked){
-            const titleTextElem = formElem.querySelector('.title-text');
-            smoothly(titleTextElem, 'textContent', this.parentNode
-            .querySelector('.option-name').textContent);
-        }
+/*     formElem.querySelectorAll('.options-container').forEach(optsElem => {
+        console.log(optsElem.querySelector('.def-options'));
     })
-});
+ */
+    titleCheckbox.addEventListener('change', function(){
+        formContElem.classList.toggle('active');
+    });
 
-nestedSensElems.forEach(nestedSensElem => {
-    nestedSensElem.addEventListener('click')
+    optionInputElems.forEach(elem => {
+        elem.addEventListener('change', function (){
+            if(this.checked){
+                const titleTextElem = formElem.querySelector('.title-text');
+                smoothly(titleTextElem, 'textContent', this.parentNode
+                .querySelector('.option-name').textContent);
+                formContElem.classList.toggle('active');
+            }
+        })
+    });
+
+    optionElems.forEach(optionElem => {
+        optionElem.addEventListener('mouseenter', function(){
+            this.classList.add('hover')
+            if(this.dataset.nested){
+                const sensElem = formElem.querySelectorAll(`[data-nested="${this.dataset.nested}"]`)[1];
+                sensElem.classList.add('act');
+                sensElem.addEventListener('mouseenter', () => {
+                    this.classList.add('hover')
+                    sensElem.classList.add('act');
+                })
+                sensElem.addEventListener('mouseleave', () => {
+                    this.classList.remove('hover')
+                    sensElem.classList.remove('act');
+                })
+            }
+            console.log(this.dataset.nested);
+        })
+        optionElem.addEventListener('mouseleave', function(){
+            this.classList.remove('hover')
+            if(this.dataset.nested){
+                const sensElem = formElem.querySelectorAll(`[data-nested="${this.dataset.nested}"]`)[1];
+                sensElem.classList.remove('act');
+            }
+        })
+    })
 })
+
+/*
+    function backgroundMove (backgrElem, top: ? = false, color: ?){
+        if(top === false){
+            backgrElem. stylelist
+        }
+    }
+*/
+
+/*
+    !: ПРОНУМЕРОВАТЬ КАЖДЫЙ option-hov, отчитывая от родителя
+ */
