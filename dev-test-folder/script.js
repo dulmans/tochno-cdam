42b12b60f3b0a9de4c802f3338b6bb1e14e12104
ulmans <abramovxz@gmail.com>
2022-07-08 04:28:59 +0300

    Finish touches in checkbox layout, codding and style

--- dev-test-folder/script.js
+++ dev-test-folder/script.js
@@ -1,64 +1,80 @@
 document.querySelectorAll('.select-app').forEach(formElem => {
     const formContElem = formElem.querySelector('.select-box')
     const titleCheckbox = formElem.querySelector('.title-box--open__options');
-    const optionsElem = formElem.querySelector('.options');
-    const optionInputElems = optionsElem.querySelectorAll('.option-input');
+    const optionsElems = formElem.querySelectorAll('.def-options');
+    const optionInputElems = optionsElems[0].querySelectorAll('.option-input');
     const optionElems = formElem.querySelectorAll('.option-hov');
+    const defaultColorHover = 'rgb(210, 210, 210)';
+    const heightOption = optionsElems[0].querySelector('.def-option').offsetHeight;
+    let summBasicOption = optionsElems[0].children.length;
 
-/*     formElem.querySelectorAll('.options-container').forEach(optsElem => {
-        console.log(optsElem.querySelector('.def-options'));
+
+    optionsElems.forEach(optionsElem => {
+        const optionsElemChild = optionsElem.children;
+        for(let i = 0; i < optionsElemChild.length; i++){
+            if(optionsElemChild[i].classList.contains('option-hov')){
+                optionsElemChild[i].setAttribute('data-numelem',i + 1);
+            }
+        }
+    }) /* Нумеруем каждый option, согласно положению в родителе */
+
+    formElem.querySelectorAll('.nested-box').forEach(nestedElem => {
+        const styleText = `${parseFloat(window.getComputedStyle(nestedElem).getPropertyValue('padding-top')) + (summBasicOption * heightOption)}px;`;
+
+        nestedElem.style.cssText = `
+            max-height: ${styleText};
+            min-height: ${styleText};
+        `;
     })
- */
+
     titleCheckbox.addEventListener('change', function(){
         formContElem.classList.toggle('active');
-    });
+    }); /* Появление элемента options, при клике на title-box */
 
     optionInputElems.forEach(elem => {
         elem.addEventListener('change', function (){
             if(this.checked){
                 const titleTextElem = formElem.querySelector('.title-text');
-                smoothly(titleTextElem, 'textContent', this.parentNode
-                .querySelector('.option-name').textContent);
-                formContElem.classList.toggle('active');
+                let res = this.parentNode.querySelector('.option-name').textContent;
+                if(this.parentNode.classList.contains('full-copy__in-title')){
+                    res = `${res} №${this.parentNode.querySelector('.option-icon').textContent}`;
+                }
+                smoothly(titleTextElem, 'textContent', res);
+                formContElem.classList.remove('active');
             }
         })
-    });
+    }); /* Изменение title элемента, согласно активному элементу */
 
     optionElems.forEach(optionElem => {
+        if(!(optionElem.dataset.colorhover)){
+            optionElem.setAttribute('data-colorhover', defaultColorHover)
+        }
+        if(optionElem.classList.contains('nested-item')){
+            const nestedElem = optionElem.querySelector('.nested-box');
+            nestedElem.style.top = `-${optionElem.dataset.numelem * heightOption}px`
+        }
         optionElem.addEventListener('mouseenter', function(){
             this.classList.add('hover')
-            if(this.dataset.nested){
-                const sensElem = formElem.querySelectorAll(`[data-nested="${this.dataset.nested}"]`)[1];
-                sensElem.classList.add('act');
-                sensElem.addEventListener('mouseenter', () => {
-                    this.classList.add('hover')
-                    sensElem.classList.add('act');
-                })
-                sensElem.addEventListener('mouseleave', () => {
-                    this.classList.remove('hover')
-                    sensElem.classList.remove('act');
-                })
-            }
-            console.log(this.dataset.nested);
+            backgroundHover(this, 1);
+
         })
         optionElem.addEventListener('mouseleave', function(){
             this.classList.remove('hover')
-            if(this.dataset.nested){
-                const sensElem = formElem.querySelectorAll(`[data-nested="${this.dataset.nested}"]`)[1];
-                sensElem.classList.remove('act');
-            }
+            backgroundHover(this, 0);
         })
     })
-})
 
-/*
-    function backgroundMove (backgrElem, top: ? = false, color: ?){
-        if(top === false){
-            backgrElem. stylelist
+    function backgroundHover (hoverElem, act){
+        const bgElem = hoverElem.parentNode.lastElementChild;
+        if(act){
+            bgElem.style.cssText = `
+                background-color: ${hoverElem.dataset.colorhover};
+                top: ${heightOption * (hoverElem.dataset.numelem - 1)}px;
+                opacity: 1;
+            `;
         }
+        else{
+            bgElem.style.cssText = ``;
+        };
     }
-*/
-
-/*
-    !: ПРОНУМЕРОВАТЬ КАЖДЫЙ option-hov, отчитывая от родителя
- */
+})
